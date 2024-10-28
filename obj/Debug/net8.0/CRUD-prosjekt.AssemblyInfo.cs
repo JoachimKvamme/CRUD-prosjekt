f10b0b807489e69b410887f7368910f90bd78c22
JoachimKvamme <kaj082024@gmail.com>
2024-10-28 14:44:33 +0100

    Migrerte og oppdaterte databasen

--- obj/Debug/net8.0/CRUD-prosjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/CRUD-prosjekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD-prosjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e077d275f8889db99b39fc4360a47764434e831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+083ff92a67869368b3dd45504f4811502b0c7150")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD-prosjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD-prosjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
