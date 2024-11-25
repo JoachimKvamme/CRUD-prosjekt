8c2cb17ca851b7eafcd9161a973de8ed51c2906f
JoachimKvamme <kaj082024@gmail.com>
2024-11-25 12:00:06 +0100

    Delte opp script-filene i wwwroot, for oversiktens skyld

---
+++ wwwroot/crud-books.js
@@ -0,0 +1,38 @@
+
+
+async function getBookData() {
+  const url = "/api/books";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function showBookData() {
+  let container = document.getElementById("bookList");
+  container.innerHTML = "";
+
+  let data = await getBookData();
+  let bookList = document.createElement("ol");
+  container.appendChild(bookList);
+  data.forEach((element) => {
+    let bookItem = document.createElement("li");
+    bookItem.innerText = `Tittel: ${element.title}, 
+      Forfatternavn: ${element.firstNameAuthor}, ${element.lastNameAuthor},
+      Utgivelsesår: ${element.year},
+      Forlag: ${element.publisher},
+      Sted: ${element.place}`;
+
+    bookList.appendChild(bookItem);
+    bookList.appendChild(document.createElement("br"));
+  });
+}
+
