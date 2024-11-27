24eaa39f170aa929ef40128a1b99647d024f5c62
JoachimKvamme <kaj082024@gmail.com>
2024-11-27 11:40:54 +0100

    fjernet frontend-react-mappen

--- obj/Debug/net8.0/CRUD-prosjekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/CRUD-prosjekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD-prosjekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0d6e059c9abe007e166f31a85d076bd0c0eba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3bdccab43cbd5844fbed307cb9520447118c0a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD-prosjekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD-prosjekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
